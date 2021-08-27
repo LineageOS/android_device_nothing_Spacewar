1bd46e43ca238fc2f2b143866a8da333fbc670c5
kubersharma001 <kubersharma001@gmail.com>
2021-08-27 17:25:10 +0000

    Spacewar: Import powerhint.json from coral
    * From: https://android.googlesource.com/device/google/coral/+/android-11.0.0_r42/powerhint.json
    
    Change-Id: I8dba2912afb27e0432bdce601ce38f2934bb0186

--- device.mk
+++ device.mk
@@ -343,6 +343,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.nothing-libperfmgr \
     android.hardware.power@1.3.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # PowerShare
 PRODUCT_PACKAGES += \
     vendor.lineage.powershare@1.0-service.nothing
