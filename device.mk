7c1090f5c806493efdba33c2e1cf0886b5dd32c2
ZVNexus <akoskovich@pm.me>
2022-02-02 14:36:49 +0530

    Spacewar: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Change-Id: Ia2cb9394fdc65148100032620aabc306f7b3c1e7

--- device.mk
+++ device.mk
@@ -356,10 +356,6 @@ PRODUCT_PACKAGES += \
     libstdc++.vendor \
     libvndfwk_detect_jni.qti.vendor
 
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
