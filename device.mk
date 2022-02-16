a925ed5be9057c740c0e0434bd5dec5efaa5a0e0
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-16 22:14:58 +0200

    Spacewar: Add dummy sensors sub HAL
    Change-Id: Ie20ed775344990de3e16e2eefba1754d831027fb

--- device.mk
+++ device.mk
@@ -359,7 +359,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
-    libsensorndkbridge
+    libsensorndkbridge \
+    sensors.nothing
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
