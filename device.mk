ccc45b33e85bdfe4678f65aca7a52636682fc52e
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    Spacewar: Inherit launch_with_vendor_ramdisk for vAB
    - Spacewar devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
