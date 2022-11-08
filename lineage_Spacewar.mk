bd07b2f7fcbb9265b78112a7d6fb26c14d0febfc
Kujou Yuko <i@779.moe>
2022-11-08 21:36:48 +0800

    Spacewar: Match product name with stock
    Change-Id: Ie822855dcab391d6144fac35639d6806e21131f1

--- lineage_Spacewar.mk
+++ lineage_Spacewar.mk
@@ -25,4 +25,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=Spacewar
+
 BUILD_FINGERPRINT := Nothing/Spacewar/Spacewar:12/SKQ1.211230.001/1666412462:user/release-keys
