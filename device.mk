0d56256899b3c785801e85ae8690cd51dfd4f033
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 08:59:13 +0300

    Spacewar: Drop libvulkan target that is built by default
    Change-Id: Ic5d73508b850fac7c7427caf12eb4a8e87e3deca

--- device.mk
+++ device.mk
@@ -193,7 +193,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     gralloc.default \
-    libvulkan \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.memtrack-service
