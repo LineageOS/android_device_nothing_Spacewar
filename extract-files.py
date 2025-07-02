81ab237ed84afe03b4f3794e93ed0a0891e1f7ac
Bruno Martins <bgcngm@gmail.com>
2025-07-03 00:14:39 +0100

    Spacewar: Reformat extract-files.py
    Command used:
    `ruff format extract-files.py --config ../../../tools/extract-utils/ruff.toml`
    
    Change-Id: I13aaee01549763b46d909a396562d0b7663c4e8d

--- extract-files.py
+++ extract-files.py
@@ -28,8 +28,10 @@ namespace_imports = [
     'vendor/qcom/opensource/display',
 ]
 
+
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
+
 
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
