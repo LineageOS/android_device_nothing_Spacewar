a45475267486987d0388a857169ce00ba11db194
BrainKub <fyhtjnt@gmail.com>
2025-10-15 22:23:19 +0300

    Revert "Spacewar: Remove QTI perfd"
    UI stutters appear with lineage-libperfmgr
    
    This reverts commit 54f61e88079df5b2e95b6d2077b58ccd141f2046.
    
    Change-Id: Ib9cb5ade6d576db63348cca3867640ba04e74418

--- device.mk
+++ device.mk
@@ -29,10 +29,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/nothing \
-    hardware/google/interfaces \
-    hardware/google/pixel \
-    hardware/qcom-caf/common/libqti-perfd-client \
-    hardware/lineage/interfaces/power-libperfmgr \
     vendor/qcom/opensource/usb/etc
 
 # A/B
@@ -284,13 +280,13 @@ PRODUCT_PACKAGES += \
 # Nt-fwk
 $(call inherit-product, hardware/nothing/nt-fwk/nt-fwk.mk)
 
+# Perf
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.2.vendor
+
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    libqti-perfd-client
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+    android.hardware.power-service-qti
 
 # PowerShare
 PRODUCT_PACKAGES += \
