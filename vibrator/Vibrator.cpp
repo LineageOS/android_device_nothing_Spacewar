6b7e6e220476b90b0347789db2c5309d551d5871
hiroshi <hiroshi@tanuki.page>
2025-11-01 06:18:59 -0400

    vibrator: richtap: Improve richtap vibration consistency
    When using the vibrator for streams (modulating amplitude over a given time),the
    amplitude can be left at an untracked value, causing prebaked effects to play at that
    incorrect amplitude. Keep track of the last used operation and max out amplitude when needed.
    
    Change-Id: Ieab88ad90e987fc9461f2f9a40007fcf642f7857

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -17,6 +17,15 @@
 #define RICHTAP_MEDIUM_STRENGTH 89
 #define RICHTAP_STRONG_STRENGTH 99
 
+enum vibrationMode {
+    MODE_NONE,
+    MODE_TIMEOUT,
+    MODE_PREBAKED,
+    MODE_STREAM,
+};
+
+static vibrationMode sLastMode = MODE_NONE;
+
 namespace aidl {
 namespace android {
 namespace hardware {
@@ -68,6 +77,7 @@ ndk::ScopedAStatus Vibrator::on(int32_t timeoutMs,
         }).detach();
     }
 
+    sLastMode = MODE_TIMEOUT;
     return ndk::ScopedAStatus::ok();
 }
 
@@ -113,6 +123,9 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es,
             return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_UNSUPPORTED_OPERATION));
     }
 
+    if (sLastMode == MODE_STREAM)
+        aac_vibra_setAmplitude(0xFF);
+
     auto mappedEffect = mapEffectToPrebakedId(effect);
     if (!mappedEffect.has_value()) {
         ALOGE("Unsupported effect: %d", static_cast<int>(effect));
@@ -138,6 +151,7 @@ ndk::ScopedAStatus Vibrator::perform(Effect effect, EffectStrength es,
 
     *_aidl_return = ret;
 
+    sLastMode = MODE_PREBAKED;
     return ndk::ScopedAStatus::ok();
 }
 
@@ -157,6 +171,7 @@ ndk::ScopedAStatus Vibrator::setAmplitude(float amplitude) {
         return ndk::ScopedAStatus(AStatus_fromExceptionCode(EX_SERVICE_SPECIFIC));
     }
 
+    sLastMode = MODE_STREAM;
     return ndk::ScopedAStatus::ok();
 }
 
