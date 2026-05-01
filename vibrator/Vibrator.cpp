6b3083ae7bb2c9c1857fd8788c4a6c98fa6c60fd
BrainKub <fyhtjnt@gmail.com>
2026-05-01 19:17:04 +0300

    Spacewar: vibrator: change vibration strength
    After remapping the values, the vibration became very weak in all modes
    
    Change-Id: I7fe5658c84a76dca1443b75f89f49f0b4dbc4d0e

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -13,9 +13,9 @@
 
 #include "aac_vibra_function.h"
 
-#define RICHTAP_LIGHT_STRENGTH 69
-#define RICHTAP_MEDIUM_STRENGTH 89
-#define RICHTAP_STRONG_STRENGTH 99
+#define RICHTAP_LIGHT_STRENGTH 80
+#define RICHTAP_MEDIUM_STRENGTH 100
+#define RICHTAP_STRONG_STRENGTH 150
 
 enum vibrationMode {
     MODE_NONE,
