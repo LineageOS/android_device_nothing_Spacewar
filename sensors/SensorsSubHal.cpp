e7b65ff5494b558314ee037b8a0e70afc9d378b0
BrainKub <fyhtjnt@gmail.com>
2026-08-18 04:14:41 +0300

    Spacewar: Implement double tap sensor
    Change-Id: I3ef6f6edefc395732598bafeb0d64e1e204d147e

--- sensors/SensorsSubHal.cpp
+++ sensors/SensorsSubHal.cpp
@@ -35,6 +35,7 @@ using ::android::hardware::sensors::V2_0::implementation::ScopedWakelock;
 SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {
     AddSensor<UdfpsSensor>();
     AddSensor<SingleTapSensor>();
+    AddSensor<DoubleTapSensor>();
 }
 
 Return<void> SensorsSubHal::getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) {
