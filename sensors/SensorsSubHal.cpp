d1dd5859dd635f61c5946a9cda3ad4c08c240208
BrainKub <fyhtjnt@gmail.com>
2026-08-19 04:15:30 +0300

    Revert "Spacewar: Add udfps long press sensor"
    This reverts commit ede249faaa557b53d62750331097c6e38cd45408.
    
    Change-Id: I48e22a94ad8cfa06c5103dcc7d078bb0550ea866

--- sensors/SensorsSubHal.cpp
+++ sensors/SensorsSubHal.cpp
@@ -32,9 +32,7 @@ namespace implementation {
 using ::android::hardware::Void;
 using ::android::hardware::sensors::V2_0::implementation::ScopedWakelock;
 
-SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {
-    AddSensor<UdfpsSensor>();
-}
+SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {}
 
 Return<void> SensorsSubHal::getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) {
     std::vector<SensorInfo> sensors;
